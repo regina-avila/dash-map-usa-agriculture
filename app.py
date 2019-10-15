bbedcbf43950461828708d09acada82ddefb37ec
ravila <regina-avila@users.noreply.github.com>
2019-10-15 18:40:01 -0400

    change column

--- app.py
+++ app.py
@@ -11,14 +11,14 @@ list_of_columns =['code', 'state', 'category', 'total exports', 'beef', 'pork', 
        'dairy', 'fruits fresh', 'fruits proc', 'total fruits', 'veggies fresh',
        'veggies proc', 'total veggies', 'corn', 'wheat', 'cotton']
 
-mycolumn='corn'
+mycolumn='dairy'
 myheading1 = f"Wow! That's a lot of {mycolumn}!"
 mygraphtitle = '2011 US Agriculture Exports by State'
 mycolorscale = 'ylorrd' # Note: The error message will list possible color scales.
 mycolorbartitle = "Millions USD"
 tabtitle = 'Old McDonald'
 sourceurl = 'https://plot.ly/python/choropleth-maps/'
-githublink = 'https://github.com/austinlasseter/dash-map-usa-agriculture'
+githublink = 'https://github.com/regina-avila/dash-map-usa-agriculture'
 
 
 ########## Set up the chart
